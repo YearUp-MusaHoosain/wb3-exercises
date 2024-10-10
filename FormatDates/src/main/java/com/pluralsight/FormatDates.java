0d85234f4b46f141ffc9f2741635e6854a0f7edf
Musa Hoosain <musahoosain@gmail.com>
2024-10-10 10:16:09 -0400

    completed Format Dates exercise

---
+++ FormatDates/src/main/java/com/pluralsight/FormatDates.java
@@ -0,0 +1,36 @@
+package com.pluralsight;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FormatDates {
+    public static void main(String[] args) {
+
+        // 09/05/2021
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDate = now.format(fmt1);
+        System.out.println(formattedDate);
+
+        // 2021-09-05
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate2 = now.format(fmt2);
+        System.out.println(formattedDate2);
+
+        //September 5, 2021
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        String formattedDate3 = now.format(fmt3);
+        System.out.println(formattedDate3);
+
+
+        DateTimeFormatter fmt4 = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss");
+        String formattedDate4 = now.format(fmt4);
+        System.out.println(formattedDate4);
+
+    }
+}
+
+//09/05/2021
+//        2021-09-05
+//September 5, 2021
+//Sunday, Sep 5, 2021 10:02
